7d0033f638a921ac8c763a29bb81316a7f8bb5b0
Yasar Mumtaz <yasar.mumtaz@innovasjonnorge.no>
2020-02-12 13:01:23 +0100

    Default editor config

---
+++ Interfaces/IMyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IN.CodingConventions.Interfaces
+{
+    public interface IMyService
+    {
+        void Method1();
+        void Method2();
+    }
+}
---
+++ Services/MyService.cs
@@ -0,0 +1,22 @@
+﻿using IN.CodingConventions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IN.CodingConventions.Services
+{
+    public class MyService : IMyService
+    {
+        public void Method1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Method2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
